747180c8816798552ec4144d7eae6c242aa78e7b
JOSE <JOSE@DESKTOP-OGALPSI>
2023-04-18 19:10:38 -0500

    Agregar archivos de proyecto.

---
+++ S3JonathanQuinapanta/Inicio.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace S3JonathanQuinapanta
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Inicio : ContentPage
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            String datoUno = txtDato1.Text;
+            String datoDos = txtDato2.Text;
+            Navigation.PushAsync(new Registro(datoUno, datoDos));
+        }
+    }
+}
---
+++ S3JonathanQuinapanta/Registro.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace S3JonathanQuinapanta
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro(string datoUno, string datoDos)
+        {
+            InitializeComponent();
+            lblDato1.Text = datoUno;
+            txtDato2.Text = datoDos;
+        }
+    }
+}
