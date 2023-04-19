5e49b88ddba1e2f7067deb68cab82c0e21d4bd3f
Jona2808 <jonathan28quina@gmail.com>
2023-04-18 19:24:55 -0500

    se agrego reportes

---
+++ S3JonathanQuinapanta/Reportes.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace S3JonathanQuinapanta
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Reportes : ContentPage
+    {
+        public Reportes()
+        {
+            InitializeComponent();
+        }
+    }
+}
